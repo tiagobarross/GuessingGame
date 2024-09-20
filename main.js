b7b0ffd903d5c5bc3109bc1332561451b6ed01cb
Tiago <tiagobarros519@gmail.com>
2024-09-19 22:52:36 -0300

    js upgrade

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
+//Var
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 let xAttempts = 1
-//Callback Function
+
+//Events
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", enter)
+
+//Functions
+function toggleScreen(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
 function handleTryClick(event){
   event.preventDefault()
   const inputNumber = document.querySelector("#inputNumber")
   if(Number(inputNumber.value) == randomNumber){
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
+    toggleScreen()
     document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
   }
   inputNumber.value = ""
   xAttempts++
 }
-//Events
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
 
-btnTry.addEventListener("click", handleTryClick)
-btnReset.addEventListener("click", function(){
+function handleResetClick(){
   location.reload()
-})
+}
+
+function enter(e){
+  if(e.key == "Enter" && screen1.classList.contains("hide")){
+    handleResetClick()
+  }
+}
