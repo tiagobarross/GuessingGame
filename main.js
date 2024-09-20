5b58e49ab47d0c3e767f630539f586c867f30de6
Tiago <tiagobarros519@gmail.com>
2024-09-19 22:20:41 -0300

    finish

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
-
-
-function handleClick(event){
+//Callback Function
+function handleTryClick(event){
   event.preventDefault()
   const inputNumber = document.querySelector("#inputNumber")
   if(Number(inputNumber.value) == randomNumber){
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
     document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
   }
+  inputNumber.value = ""
   xAttempts++
 }
+//Events
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 
-function tryAgain(){
-  document.querySelector(".screen1").classList.remove("hide")
-  document.querySelector(".screen2").classList.add("hide")
-}
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", function(){
+  location.reload()
+})
