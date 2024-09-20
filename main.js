186dd51df7d3dbe7a68deaba8d745661192a51d6
Tiago <tiagobarros519@gmail.com>
2024-09-19 21:41:09 -0300

    js init

---
+++ main.js
@@ -0,0 +1,19 @@
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+
+function handleClick(event){
+  event.preventDefault()
+  const inputNumber = document.querySelector("#inputNumber")
+  if(Number(inputNumber.value) == randomNumber){
+    document.querySelector(".screen1").classList.add("hide")
+    document.querySelector(".screen2").classList.remove("hide")
+    document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
+  }
+  xAttempts++
+}
+
+function tryAgain(){
+  document.querySelector(".screen1").classList.remove("hide")
+  document.querySelector(".screen2").classList.add("hide")
+}
